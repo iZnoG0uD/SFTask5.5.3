318017f5fe40216c93db285839ff3268b3fcedef
iZnoGouD <zelesnac@gmail.com>
2023-10-09 23:47:38 +0400

    Добавьте файлы проекта.

---
+++ SFTask5.5.3/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace SFTask5._5._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Напишите что-то");
+            var str = Console.ReadLine();
+
+            Console.WriteLine("Укажиет глубину эха");
+            var deep = Convert.ToInt32(Console.ReadLine());
+
+            Echo(str, deep);
+
+            Console.ReadKey();
+        }
+
+        static void Echo(string phrase, int deep)
+        {
+            var modif = phrase;
+
+            if (modif.Length > 2)
+                modif = modif.Remove(0, 2);
+
+            Console.BackgroundColor = (ConsoleColor)deep;
+            Console.WriteLine("..." + modif);
+
+            if (deep > 1)
+                Echo(modif, deep - 1);
+        }
+    }
+}
